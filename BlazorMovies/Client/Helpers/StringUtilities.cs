7b62bd8b110bcae6806290da0d295bef436eb9b5
bosnjaknikola <nikola.bosnjak@wayseven.com>
2020-02-04 16:47:33 +0100

    initial commit

---
+++ BlazorMovies/Client/Helpers/StringUtilities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Client.Helpers
+{
+    public class StringUtilities
+    {
+        public static string CustomeToUpper(string value) => value.ToUpper();
+            
+    }
+}
